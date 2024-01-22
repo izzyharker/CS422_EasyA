6212990f3b85dbaff63c0b9bf46d91dc333ab1f0
Izzy Harker <harkerisabella@gmail.com>
2024-01-22 15:18:11 -0800

    ReadGradeData now works with original gradedata.js file. Also added somecomments

--- ReadGradeData.py
+++ ReadGradeData.py
@@ -4,10 +4,8 @@ Date: 1/21/24
 Description: Contains a class which reads data from the gradedata.txt file into a dictionary
 
 Notes: 
-- gradedata.txt is the same as gradedata.js, I just deleted the function at the end for my sanity
-- this should work with any similar files, as long as they maintain json-like formatting. specifically, 
-    as long as attributes are in the form [key]:[val] and on the same line, it should be fine
-- i had other notes but i am forgetting them. anyway
+- now works with original gradedata.js file
+- breaks on keyword "function" keyword to indicate end of data
 """
 
 class readGradeData():
@@ -21,21 +19,31 @@ class readGradeData():
         return self.data
 
     def read_data(self):
+        # open file and read lines
         raw_data = open(self.filepath, "r")
         raw_data_lines = raw_data.readlines()
 
         raw_data.close()
 
+        # set current key and index to default values
         current_key = ""
         crn_index = 0
 
+        # iterate through lines of file
         for raw_line in raw_data_lines:
+            # strip newline and , chars
             current_line = raw_line.strip().strip(",\n")
 
+            # check if we've reached the end of the data
+            if "function" in current_line:
+                break
+
+            # split line by : and remove spaces, whitespace
             current_line_data = []
             for token in current_line.split(":"):
                 current_line_data.append(token.replace("'", "").strip())
 
+            # if no : found and we are between class codes
             if len(current_line_data) == 1:
                 if current_key != "":
                     for char in current_line_data[0]:
@@ -46,7 +54,9 @@ class readGradeData():
                             crn_index += 1
                         elif char == "]":
                             current_key = ""
+            # otherwise, we have a data entry!
             elif len(current_line_data) == 2:
+                # if we find a new class code, process it
                 if current_key == "":
                     current_key = current_line_data[0]
                     crn_index = 0
@@ -58,10 +68,17 @@ class readGradeData():
                             crn_index += 1
                         elif char == "]":
                             current_key = ""
+                # otherwise enter data into current class code
                 else:
                     self.data[current_key][crn_index - 1][current_line_data[0]] = current_line_data[1]
 
     def reformatForTermPrio(self):
+        """
+        Reformats data dictionary to have term as the top level
+        
+        Returns:
+            Reformatted dictionary
+        """
         reformatted_data = {}
         for classid, sections in self.data.items():
             for section in sections:
@@ -77,8 +94,7 @@ class readGradeData():
 
 """
 Testing code for the class
-
-grade_data_container = readGradeData("gradedata.txt")
+grade_data_container = readGradeData("gradedata.js")
 grade_data = grade_data_container.get_data()
 
 print(grade_data_container.data["CIS415"])
