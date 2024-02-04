be60e634ce8e0b5aed778a2d62079114224106e6
José Renteria <120423283+joserenter1a@users.noreply.github.com>
2024-02-04 15:44:05 -0800

    Update UI_loop.py

--- Modules/UI_loop.py
+++ Modules/UI_loop.py
@@ -6,7 +6,6 @@ README: Main container for all UI components that get generated. Components are 
 notes: If you want to see the functions, see ui.py
 """
 
-from Modules.data import class_data, years, courses
 from Modules.ui import *
 from Modules.GenerateGraph import GenerateGraph
 
